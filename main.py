a9b67d725a4ed4e96d96c2134cc6845e39550bc8
Un1Q <rokasjodka1@gmail.com>
2022-08-18 11:59:05 +0100

    Added check to make sure countries for given index exist

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
+import sys
 import easygui
 import os
 
 #generates a list to be populated with selected csv files
 country_data = []
-change_data = []
+user_data = []
 
 
 def reference_read():
@@ -35,7 +36,7 @@ def data_read():
             with open(path) as data:
                 reader = csv.reader(data)
                 for item in reader:
-                    change_data.append(item)
+                    user_data.append(item)
         except FileNotFoundError:
             directory = os.getcwd()
             print(f"File not found in {directory}/{path}, please input new file location")
@@ -51,49 +52,59 @@ def run():
     print(" ")
     print("Opening change data")
     data_read()
-    length = data_length(change_data)
+    length = data_length(user_data)
+
     print(f"There are {length-1} items in the data set")
     print("Change data success\n\n")
+    print("What format are the countries in your .csv file?")
+    print("Full name (Brazil) [1]")
+    print("Alpha-2 (PL) [2]")
+    print("Alpha-3 (LTU) [3]")
+    print("Country Code (050) [4]")
+    print("ISO-3166-2 (ISO 3166-2:BE) [5]")
 
     while True:
+        try:
+            country_format = int((input("User Input: ")))
+            if country_format not in {1, 2, 3, 4, 5}:
+                print("Wrong input")
+        except ValueError:
+            print("Wrong value")
+        else:
+            break
 
-        print("What format are the countries in your .csv file?")
-        print("Full name (Brazil) [1]")
-        print("Alpha-2 (PL) [2]")
-        print("Alpha-3 (LTU) [3]")
-        print("Country Code (050) [4]")
-        print("ISO-3166-2 (ISO 3166-2:BE) [5]")
-
-        while True:
-            try:
-                country_format = int((input("User Input: ")))
-                if country_format not in {1, 2, 3, 4, 5}:
-                    print("Wrong input")
-            except ValueError:
-                print("Wrong value")
-            else:
+    print("Please enter the index of the country in your csv file")
+    while True:
+        try:
+            country_index_location = int(input("User input: "))
+        except ValueError:
+            print("Please enter valid index")
+        else:
+            country_exists, total_matching = check_country_existence(country_index_location, country_format)
+            if country_exists:
+                print(f"Successfully matched {total_matching} countries out of {data_length(user_data)-1} items in the data set")
                 break
-
-        while True:
-            try:
-                country_index_location = int(input("Please enter the index of the country in your csv file: "))
-                if check_country_existence(country_index_location, country_format):
-                    continue
-                else:
-                    print("No matching countries found for given index.")
-
-            except ValueError:
-                print("Please enter valid index")
             else:
-                break
+                sys.exit("No matching countries found for given index.")
+
+
+
+
+
+def region_adder():
+    pass
 
 
 def check_country_existence(country_index_location, country_format):
-
-    for i in range(data_length(country_data)):
-        for j in range(data_length(change_data)):
-            if country_data[country_format-1][i] == change_data[country_index_location][j]:
-                return True
+    total_matching_countries = 0
+    for i in range(1, data_length(country_data)):
+        for j in range(1, data_length(user_data)):
+            if country_data[i][country_format-1] == user_data[j][country_index_location]:
+                total_matching_countries += 1
+    if total_matching_countries > 0:
+        return True, total_matching_countries
+    else:
+        return False, total_matching_countries
 
 
 def data_length(data):
