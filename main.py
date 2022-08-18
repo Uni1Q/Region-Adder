b499b1a40663a597f9706ab0f661a40f1b66993a
Un1Q <rokasjodka1@gmail.com>
2022-08-18 12:17:33 +0100

    added selection of items to add

--- main.py
+++ main.py
@@ -48,8 +48,7 @@ def data_read():
 def run():
     print("Opening reference data")
     reference_read()
-    print("Reference data success")
-    print(" ")
+    print("Reference data success\n\n")
     print("Opening change data")
     data_read()
     length = data_length(user_data)
@@ -82,13 +81,42 @@ def run():
         else:
             country_exists, total_matching = check_country_existence(country_index_location, country_format)
             if country_exists:
-                print(f"Successfully matched {total_matching} countries out of {data_length(user_data)-1} items in the data set")
+                print(f"Successfully matched {total_matching} countries out of {data_length(user_data)-1} items in the data set\n\n")
                 break
             else:
                 sys.exit("No matching countries found for given index.")
 
+    print("What would you like to add to your data set?\n")
+    print("Note - Items will be added in the order selected\n")
+    print("Full name        (Portugal)          [1]")
+    print("Alpha-2          (AT)                [2]")
+    print("Alpha-3          (BGR)               [3]")
+    print("Country Code     (208)               [4]")
+    print("ISO-3166-2       (ISO 3166-2:DO)     [5]")
+    print("Region           (Oceania)           [6]")
+    print("Sub-Region       (South America)     [7]")
+    print("Region Code      (009)               [8]")
+    print("Sub-Region Code  (154)               [9]")
+    print("Quit             (Quit)              [0]\n")
 
+    added_details = []
+    while True:
+        try:
+            user_choice = int(input("User Input: "))
+            if user_choice == 0:
+                break
+            elif user_choice in range(1, 10):
+                if user_choice in added_details:
+                    print("Item is already in the list")
+                else:
+                    added_details.append(user_choice)
+            else:
+                print("Invalid Input")
 
+        except ValueError:
+            print("Wrong value")
+
+    region_adder()
 
 
 def region_adder():
